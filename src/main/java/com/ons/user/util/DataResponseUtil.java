74dba0e755927778462b477bb53cf90b286034c3
Aiden O'Neill <aiden@oneillsoftware.com>
2025-02-20 12:46:23 +0000

    Opal Spike - Added opal client and account check

--- src/main/java/com/ons/user/util/DataResponseUtil.java
+++ src/main/java/com/ons/user/util/DataResponseUtil.java
@@ -1,6 +1,7 @@
-package com.ons.ping.util;
+package com.ons.user.util;
 
-import com.ons.ping.dto.DataResponse;
+import com.ons.user.dto.DataResponse;
+import com.ons.user.dto.Account;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -9,9 +10,12 @@ public class DataResponseUtil {
     public static DataResponse createDataResponse() {
         Map<String, DataResponse.User> users = new HashMap<>();
         Map<String, DataResponse.RolePermission[]> rolePermissions = new HashMap<>();
+        Map<String, Account> accounts = new HashMap<>();
 
         users.put("alice", DataResponse.User.builder()
+                .name("Alice")
                 .roles(new String[]{"admin"})
+                .accountIds(new String[]{"account1", "account2"})
                 .location(DataResponse.Location.builder()
                         .country("US")
                         .ip("8.8.8.8")
@@ -19,7 +23,9 @@ public class DataResponseUtil {
                 .build());
 
         users.put("aiden", DataResponse.User.builder()
+                .name("Aiden")
                 .roles(new String[]{"employee", "billing"})
+                .accountIds(new String[]{"account3"})
                 .location(DataResponse.Location.builder()
                         .country("US")
                         .ip("8.8.8.8")
@@ -27,7 +33,9 @@ public class DataResponseUtil {
                 .build());
 
         users.put("sunil", DataResponse.User.builder()
+                .name("Sunil")
                 .roles(new String[]{"guest"})
+                .accountIds(new String[]{"account2"})
                 .location(DataResponse.Location.builder()
                         .country("US")
                         .ip("8.8.8.8")
@@ -35,7 +43,9 @@ public class DataResponseUtil {
                 .build());
 
         users.put("eve", DataResponse.User.builder()
+                .name("Eve")
                 .roles(new String[]{"customer"})
+                .accountIds(new String[]{"account5"})
                 .location(DataResponse.Location.builder()
                         .country("US")
                         .ip("8.8.8.8")
@@ -66,9 +76,22 @@ public class DataResponseUtil {
                 DataResponse.RolePermission.builder().action("read").type("finance").build()
         });
 
+        accounts.put("account1", Account.builder()
+                .accountId("account1")
+                .accountNumber("12345678")
+                .sortCode("12-34-56")
+                .build());
+
+        accounts.put("account2", Account.builder()
+                .accountId("account2")
+                .accountNumber("87654321")
+                .sortCode("65-43-21")
+                .build());
+
         return DataResponse.builder()
                 .users(users)
                 .role_permissions(rolePermissions)
+                .accounts(accounts)
                 .build();
     }
 }
